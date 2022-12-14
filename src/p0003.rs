445edbaad28b68c297718d8cdc32074b764992e3
Michael Siegwarth <msiegwarth@gmail.com>
2022-12-14 12:46:09 -0300

    "Rustified" code

--- src/p0003.rs
+++ src/p0003.rs
@@ -20,24 +20,20 @@ pub fn go() {
     // let n: u64 = 13195;
     let n: u64 = 600851475143;
 
-    let mut largest_prime_factor;
     let mut multiplied_prime_factors = 1;
     let mut i = 1;
 
-    loop {
+    let largest_prime_factor = loop {
         if n % i == 0 && is_prime(i) {
-            largest_prime_factor = i;
             multiplied_prime_factors *= i;
 
-            // All prime factors multiplied must 
-            // have the same value as N
             if n == multiplied_prime_factors {
-                break
+                break i
             };
         }
 
         i += 2;
-    }
+    };
 
     println!("P0003 result: {}", largest_prime_factor);
 }
