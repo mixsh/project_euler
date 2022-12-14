a713a3f374e2719b7624e51c43548d917ae5df28
Michael Siegwarth <msiegwarth@gmail.com>
2022-12-13 22:31:04 -0300

    Problem 0001 - Very pragmatic

---
+++ p0001/src/main.rs
@@ -0,0 +1,33 @@
+//! PROBLEM 0001
+//! Author: Michael
+//! 
+//! If we list
+//! all the natural numbers
+//! below 10 
+//! that are multiples of 3 or 5
+//! we get 3, 5, 6 and 9.
+//! The sum
+//! of these multiples
+//! is 23.
+//! Find the sum
+//! of all the multiples of 3 or 5
+//! below 1000.
+
+fn main() {
+    let mut n = 3;
+    let mut total = 0;
+
+    loop {
+        if n % 3 == 0 || n % 5 == 0 {
+            total += n;
+        }
+        
+        n += 1;
+
+        if n >= 1000 {
+            break;
+        }
+    }
+    
+    println!("Result: {}", total);
+}
